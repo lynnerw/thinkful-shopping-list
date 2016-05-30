ddb4e7dcddf7fb1b4126136380825f32a897a792
lynnerw <larwitter@gmail.com>
2016-05-30 18:51:42 -0400

    updated draft changes click event to mousedown

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 $("#inventory-list li").uniqueSort() {
       order: 'asc'; */
 
-  /* if click occurs in inventory-list, move it to shopping-list with border-top value */
-$("#inventory-list .store-area-1 li").click(function(event) {
+  /* if click occurs in inventory-list, move item to shopping-list */
+$("#inventory-list li").mousedown(function() {
   $(this).clone().appendTo("#shopping-list ul")
   $(this).toggleClass("item-selected");
 });
@@ -14,13 +14,10 @@ $("#inventory-list .store-area-1 li").click(function(event) {
   .clone().appendTo("#shopping-list")
   .toggleClass("store-area-2 li");
 }); */
-/* $("#inventory-list .store-area-3 li").click(function(event) {
-  $(this).toggleClass("item-selected")
-  .clone().appendTo("#shopping-list")
-  .toggleClass("#shopping-list.store-area-3 li");
-}); */
-$("#shopping-list li").click(function(event) {
-  /* $(this).clone().appendTo("#in-cart ul") */
+
+/* if click occurs in shopping-list, move item to in-cart */
+$("#shopping-list li").mousedown(function() {
+  $(this).clone().appendTo("#in-cart ul")
   console.log("shopping list item was clicked");
 });
 });
