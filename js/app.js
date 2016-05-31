6e2672ff323463d0adb2ee7907bffc04270eaa28
lynnerw <larwitter@gmail.com>
2016-05-30 20:41:14 -0400

    enhancements to shopping lit

--- js/app.js
+++ js/app.js
@@ -3,21 +3,30 @@ $(document).ready(function() {
 $("#inventory-list li").uniqueSort() {
       order: 'asc'; */
 
-  /* if click occurs in inventory-list, move item to shopping-list */
-$("#inventory-list li").mousedown(function() {
-  $(this).clone().appendTo("#shopping-list ul")
+  /* if mousedown occurs in inventory-list area 1, move item to shopping-list */
+$("#inventory-list .store-area-1 li").mousedown(function() {
+  $(this).clone().appendTo("#shopping-list .store-area-1 ul")
+  /*.toggleClass("store-area-1.li") */
   $(this).toggleClass("item-selected");
 });
 
-/* $("#inventory-list .store-area-2 li").click(function(event) {
-  $(this).toggleClass("item-selected")
-  .clone().appendTo("#shopping-list")
-  .toggleClass("store-area-2 li");
-}); */
+/* if mousedown occurs in inventory-list area 2, move item to shopping-list */
+$("#inventory-list .store-area-2 li").mousedown(function() {
+   $(this).clone().appendTo("#shopping-list .store-area-2 ul")
+  /* .toggleClass("store-area-2 li") */
+  $(this).toggleClass("item-selected");
+});
 
-/* if click occurs in shopping-list, move item to in-cart */
-$("#shopping-list li").mousedown(function() {
-  $(this).clone().appendTo("#in-cart ul")
+/* if mousedown occurs in inventory-list area 3, move item to shopping-list */
+$("#inventory-list .store-area-3 li").mousedown(function() {
+   $(this).clone().appendTo("#shopping-list .store-area-3 ul")
+   /* .toggleClass("store-area-3 li") */
+  $(this).toggleClass("item-selected");
+});
+
+/* if mousedown occurs in shopping-list, hide item in shopping list and show in in-cart */
+$("#shopping-list > li").mousedown(function() {
+/*  $(this).clone().appendTo("#in-cart ul") */
   console.log("shopping list item was clicked");
 });
 });
