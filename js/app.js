fc1807d0b966c02fe6387bbe836e76cbb9efc105
lynnerw <larwitter@gmail.com>
2016-06-05 10:32:24 -0400

    still trying to get at least one thing to work

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+  document.getElementById("new-area1-item").addEventListener('keypress', function(e) {
+         if (e.keyCode == 13) {
+           var newItem = $("#new-area1-item").val();
+           $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-1 ul").html(newItem);
+          e.preventDefault();
+          $.val("#new-area1-item").hide();
+         }
+  });
+
+  document.getElementById("new-area2-item").addEventListener('keypress', function(e) {
+         if (e.keyCode == 13) {
+           var newItem = $("#new-area2-item").val();
+           $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-2 ul");
+           console.log(newItem);
+             e.preventDefault();
+         }
+  });
+
+  document.getElementById("new-area3-item").addEventListener('keypress', function(e) {
+         if (e.keyCode == 13) {
+           var newItem = $("#new-area3-item").val();
+           $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-3 ul");
+           console.log(newItem);
+             e.preventDefault();
+         }
+  });
 /* if mousedown occurs in inventory-list area 1, move item to shopping-list */
 $("#inventory-list .store-area-1 li").mousedown(function() {
   $(this).clone().appendTo("#shopping-list .store-area-1 ul")
@@ -25,41 +51,16 @@ $("#inventory-list .store-area-3 li").mousedown(function() {
 }); */
 
 $(function() {
-    $( "#in-cart" ).sortable({
+    $("#in-cart ul").sortable({
       revert: true
     });
-    $( "#shopping-list li" ).draggable({
-      connectToSortable: "#in-cart",
+    $("#shopping-list .store-area-1 ul li").draggable({
+      connectToSortable: "#in-cart ul",
       helper: "clone",
       revert: "invalid"
     });
-    $( "ul, li" ).disableSelection();
+/*    $( "ul, li" ).disableSelection(); */
   });
 
-document.getElementById("new-area1-item").addEventListener('keypress', function(e) {
-       if (e.keyCode == 13) {
-         var newItem = $("#new-area1-item").val();
-         $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-1 ul");
-         console.log(newItem);
-           e.preventDefault();
-       }
-});
 
-document.getElementById("new-area2-item").addEventListener('keypress', function(e) {
-       if (e.keyCode == 13) {
-         var newItem = $("#new-area2-item").val();
-         $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-2 ul");
-         console.log(newItem);
-           e.preventDefault();
-       }
-});
-
-document.getElementById("new-area3-item").addEventListener('keypress', function(e) {
-       if (e.keyCode == 13) {
-         var newItem = $("#new-area3-item").val();
-         $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-3 ul");
-         console.log(newItem);
-           e.preventDefault();
-       }
-});
 });
